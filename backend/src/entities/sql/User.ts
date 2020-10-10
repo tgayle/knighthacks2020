33c6a2853f1617b173c3ede6e276ea14deadc84e
tgayle <travisgayledev@gmail.com>
2020-10-09 21:30:39 -0400

    validate education level in registration

--- backend/src/entities/sql/User.ts
+++ backend/src/entities/sql/User.ts
@@ -7,6 +7,20 @@ import {
 } from "typeorm";
 import { CONN_SQL } from "../../db";
 
+export enum SchoolLevel {
+  HS_FRESH = "hs_fresh",
+  HS_SOPH = "hs_soph",
+  HS_JUNIOR = "hs_junior",
+  JS_SENIOR = "hs_senior",
+
+  C_FRESH = "c_fresh",
+  C_SOPH = "c_soph",
+  C_JUNIOR = "c_junior",
+  C_SENIOR = "c_senior",
+
+  NONE = "none",
+}
+
 @Entity({ database: CONN_SQL })
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn("uuid")
@@ -32,4 +46,7 @@ export default class User extends BaseEntity {
 
   @Column({ nullable: true, type: "integer" })
   satScore: number | null = null;
+
+  @Column()
+  mostEducation: SchoolLevel = SchoolLevel.NONE;
 }
--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,12 +1,44 @@
 import { Router } from "express";
 import { getConnection } from "typeorm";
 import { CONN_SQL } from "../db";
-import User from "../entities/sql/User";
+import User, { SchoolLevel } from "../entities/sql/User";
 
 const router = Router();
 
 router.post("/register", async (req, res) => {
-  res.json(await getConnection(CONN_SQL).getRepository(User).find());
+  const {
+    email,
+    ethnicity,
+    firstName,
+    lastName,
+    mostEducation,
+    gpa,
+    satScore,
+  } = req.body as Partial<User>;
+
+  try {
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    if (!firstName || !lastName) {
+      throw new Error("First and last name are required.");
+    }
+
+    if (!mostEducation || !Object.values(SchoolLevel).includes(mostEducation)) {
+      throw new Error(
+        `A level of education must be specified: [${Object.values(
+          SchoolLevel
+        ).join(", ")}]`
+      );
+    }
+
+    res.send("yes");
+  } catch (e) {
+    res.status(400).json({
+      message: e.message,
+    });
+  }
 });
 
 export default router;
